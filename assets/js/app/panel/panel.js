09150f4a712ab7ea6972f693b29137bad2d8d2a9
vaot <victorandrey_123@hotmail.com>
2018-07-21 17:23:00 -0700

    Clean this up. We will tackle emoji after encryption

--- assets/js/app/panel/panel.js
+++ assets/js/app/panel/panel.js
@@ -1,9 +1,5 @@
 let app = angular.module('chatty');
 
-import { load, parse } from 'gh-emoji';
-
-window.parse = parse;
-
 app.directive('panel', [
   'RoomManager',
   (RoomManager) => {
@@ -27,40 +23,24 @@ app.directive('panel', [
 
           controller.onKeyPress = (message, event) => {
             if (event.keyCode == ENTER_KEY_CODE) {
-              controller.sendMessage(message);
-              // console.log("TESTING THIS AREA");
+              controller.sendMessage(message)
             }
           }
 
-          controller.onMessageChange = (message) => {
-            load().then(() => {
-              let pm = parse(message);
-              console.log(pm);
-              message = pm;
-              const test = pm.replace("<img","<img draggable='false' (dragstart)='false;' class='unselectable'");
-              // let first_message = message;
-              // let parsed_message = parse(message);
-              // console.log("first: " +first_message);
-              // console.log("parsed: " +parsed_message);
-              $scope.message = test.replace("<br>", "");
-            });
-            // console.log(message);
-          }
-
           controller.setup = () => {
-            $scope.messages = [];
-            $scope.attr = {};
-            $scope.attr.userId = window.parseInt(window.Chatty.userId, 10);
+            $scope.messages = []
+            $scope.attr = {}
+            $scope.attr.userId = window.parseInt(window.Chatty.userId, 10)
 
             RoomManager.on('message', (payload) => {
               $scope.$apply(() => {
-                $scope.messages.push(payload);
+                $scope.messages.push(payload)
               })
             })
           }
 
           controller.setup()
-          return controller;
+          return controller
         }
       ],
       controllerAs: 'panelCtrl',
@@ -68,28 +48,3 @@ app.directive('panel', [
     }
   }
 ]);
-
-app.directive('contenteditable', () => {
-      return {
-        require: 'ngModel',
-        restrict: 'A',
-        link: (scope, elm, attr, ngModel) => {
-
-          let updateViewValue = (event) => {
-            ngModel.$setViewValue(event.currentTarget.innerHTML);
-          }
-          //Binding it to keyup, lly bind it to any other events of interest 
-          //like change etc..
-          elm.on('keyup', updateViewValue);
-
-          scope.$on('$destroy', () => {
-            elm.off('keyup', updateViewValue);
-          });
-
-          ngModel.$render = () => {
-             elm.html(ngModel.$viewValue);
-          }
-
-        }
-    }
-});
